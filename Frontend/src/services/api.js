992617a442d6e70d28002cc83b4892392476dbc3
Shruti Kolpyakwar <shrutikolpyakwar@gmail.com>
2026-06-19 13:26:37 +0530

    make changes in assitant

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,16 +1,12 @@
 import axios from "axios";
 
-export const sendMessage = async (
-  symptom
-) => {
-
-  const response =
-    await axios.post(
-      "https://the-health-card-chatassistant.onrender.com/api/chat",
-      {
-        symptom
-      }
-    );
+export const sendMessage = async (symptom) => {
+  const response = await axios.post(
+    "http://localhost:5000/api/chat",
+    {
+      symptom
+    }
+  );
 
   return response.data;
 };
